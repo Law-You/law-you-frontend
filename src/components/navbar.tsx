2e49246ed0e0cf56e27bbd6b7531f3edf0739508
Abinandhan Shanmugasundaram <info@lawyou.org>
2025-11-19 00:35:23 +0530

    Changed the navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './navbar-clean.css';
 
@@ -12,36 +11,57 @@ const Navbar = () => {
         <span className="blue">LAW</span>
         <span className="black"> YOU</span>
       </div>
+
       {/* Navigation links on the right */}
       <nav className="nav-links">
         <Link
           to="/"
           className={`nav-link${location.pathname === '/' ? ' active' : ''}`}
-        >HOME</Link>
+        >
+          HOME
+        </Link>
+
         <Link
           to="/for-client"
           className={`nav-link${location.pathname === '/for-client' ? ' active' : ''}`}
-        >FOR CLIENT</Link>
+        >
+          FOR CLIENT
+        </Link>
+
         <Link
           to="/for-lawyers"
           className={`nav-link${location.pathname === '/for-lawyers' ? ' active' : ''}`}
-        >FOR LAWYERS</Link>
+        >
+          FOR LAWYERS
+        </Link>
+
         <Link
           to="/admin"
           className={`nav-link${location.pathname === '/admin' ? ' active' : ''}`}
-        >ADMIN</Link>
+        >
+          ADMIN
+        </Link>
+
         <Link
           to="/legal-topics"
           className={`nav-link${location.pathname === '/legal-topics' ? ' active' : ''}`}
-        >LEGAL TOPICS</Link>
+        >
+          LEGAL TOPICS
+        </Link>
+
         <Link
           to="/login"
           className={`nav-link${location.pathname === '/login' ? ' active' : ''}`}
-        >Log in</Link>
+        >
+          Log in
+        </Link>
+
         <Link
           to="/book-consultation"
           className="nav-button"
-        >Book Consultation</Link>
+        >
+          Book Consultation
+        </Link>
       </nav>
     </div>
   );
